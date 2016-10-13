b83fd6983b0111f4b49a12f71097040257c953af
knoddin <knoddin@gmail.com>
2016-10-13 13:53:02 -0400

    Read the file to be uploaded.
    Begin the upload script, tested to make sure we can get filename and
    data back from fs.readfile.
    
    Use empty string as default argument for the filepath so if we don't
    pass an argument when running the script, we get a sensible error
    message (file not found).

---
+++ bin/s3-upload.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node -i
+
+'use strict';
+
+
+const fs = require('fs');
+
+const filename = process.argv[2] || '';
+
+fs.readFile(filename, (error, data) => {
+  if (error) {
+    return console.error(error);
+  }
+
+  console.log(`${filename} is ${data.length} bytes long`);
+});
